0f5b2fd40884ff3e4d9a0399ac2ff603994a7411
Gianluca Chiap <gianlucachiap@gmail.com>
2018-09-28 18:41:24 +0200

    the power of innovation review

--- src/pages/book/introduction/the-power-of-innovation/index.js
+++ src/pages/book/introduction/the-power-of-innovation/index.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import { Paragraph, Page, ChapterTitle } from 'components';
-import PageTemplate from 'templates/PageTemplate';
 
 const ThePowerOfInnovation = () => (
   <Page>
     <ChapterTitle>The power of innovation</ChapterTitle>
     <Paragraph>
-      In 1975 Steven Sasson, a Kodak engineer realised the first self-contained
-      digital camera. Kodak told him to keep the invention private, worried that
-      digital cameras could end up having a negative impact on their core
+      In 1975 Steven Sasson, an engineer of the Eastman Kodak company, created
+      what is considered to be the first digital camera. The company, at the
+      time a leader in the chemical and film industry, told him to keep this
+      invention private, worried about potential negative impacts on his
       business.
     </Paragraph>
     <Paragraph>
-      In 2007 half of the mobile-phones in the world were produced by Nokia.
-      When Apple and Google came out with iOS and Android, Nokia sticked with
-      its Symbian OS, failing to understand the central role of software in
-      smartphones. Similarly, Blackberry kept thinking that using half of the
-      phone size for a keyboard was a great idea.
+      In 2007, Nokia was close to control 50% market share in the mobile phone
+      industry (Source Gartner). Soon after, when Apple and Google started out
+      with iOS and Android OS, Nokia did not understand the central role of
+      software in smartphones, choosing not to concentrate their efforts on the
+      evolution of their Symbian OS. In the meantime, Blackberry carry on the
+      idea of using half of the mobile phone size for the keyboard. We all know
+      how it ended.
     </Paragraph>
     <Paragraph>
-      History, especially in the digital era, is crowded with dominant companies
-      that ended up losing big opportunities, got heavily resized or declared
-      bankruptcy because of their failure to truly understand the innovation a
-      new technology or a new idea could bring. New ideas have the power to
-      destroy entire markets in a matter of years, just like Napster destroyed
-      Tower Records by simply showing that it was possible to share music files
-      between people. Napster was shut down, but once the world understood the
-      innovation behind it, nobody wanted to open a record store again. Uber
-      created a new business-model, and even if regulators are trying to stop
-      it, the idea of becoming a taxi driver is not very appealing anymore.
+      History, especially in the digital age, is full of dominant societies that
+      have ended up losing big opportunities, have been heavily downsized or
+      failed due to their inability to fully understand the innovation that a
+      new technology or new idea could have bring. New ideas have the power to
+      destroy entire markets within a few years, just as Napster destroyed Tower
+      Records simply by showing that it was possible to share music files
+      between people. Napster has been closed, but once the world has understood
+      the innovation behind it, no one has ever considered a record store as a
+      profitable investment. Uber has created a new business model and, although
+      regulators are trying to stop it, the idea of becoming a taxi driver is no
+      longer very tempting.
     </Paragraph>
     <Paragraph>
       Innovation can’t be stopped, it is not possible to un-invent the internet,
