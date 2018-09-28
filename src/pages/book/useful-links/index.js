c2573fe053bd54cb325dde2261074c69637bf905
Gianluca Chiap <gianlucachiap@gmail.com>
2018-09-28 11:59:59 +0200

    useful links page

---
+++ src/pages/book/useful-links/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  Paragraph,
+  Page,
+  Important,
+  ChapterTitle,
+  SubChapterTitle,
+  ImageWrapper,
+  Italic,
+  Bold,
+} from 'components';
+import PageTemplate from 'templates/PageTemplate';
+
+const UsefulLinks = () => (
+  <PageTemplate>
+    <Page>
+      <ChapterTitle>Useful links</ChapterTitle>
+    </Page>
+  </PageTemplate>
+);
+
+export default UsefulLinks;
